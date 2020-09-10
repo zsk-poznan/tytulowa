e74f382199f60ccaaeb5cc0a068061b8e3669018
datejer <ejer.priv@wp.pl>
2020-09-10 22:57:10 +0200

    Updates

---
+++ precache-manifest.795b20d4df686e1fda562d28ecfc2006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a375ae7618dd329d1f9d5033456d3703",
+    "url": "/tytulowa/index.html"
+  },
+  {
+    "revision": "b38e82747167a85e84e0",
+    "url": "/tytulowa/static/css/main.cb07c542.chunk.css"
+  },
+  {
+    "revision": "7ef21c04e63408f158d2",
+    "url": "/tytulowa/static/js/2.4f61f20a.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/tytulowa/static/js/2.4f61f20a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38e82747167a85e84e0",
+    "url": "/tytulowa/static/js/main.42216028.chunk.js"
+  },
+  {
+    "revision": "0a8bc2b7b9fbf898a878",
+    "url": "/tytulowa/static/js/runtime-main.e84f48b5.js"
+  }
+]);
