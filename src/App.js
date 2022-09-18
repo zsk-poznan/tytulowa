3f4d4ea7c48d64c96a6170b50e20c72d285d7e6d
ejer <ejer.priv@wp.pl>
2022-09-18 13:01:39 +0200

    Add subject toggle

--- src/App.js
+++ src/App.js
@@ -4,64 +4,72 @@ import useCanvas from "./hooks/useCanvas";
 import "./App.css";
 
 function App() {
-	const [name, setName] = useState("Jan Kowalski");
-	const [klasa, setKlasa] = useState("1X");
+  const [subject, setSubject] = useState("geo");
+  const [name, setName] = useState("Jan Kowalski");
+  const [klasa, setKlasa] = useState("1X");
 
-	const canvasRef = useRef(null);
+  const canvasRef = useRef(null);
 
-	useCanvas(canvasRef, name, klasa);
+  useCanvas(canvasRef, name, klasa, subject);
 
-	const save = (e) => {
-		const img = canvasRef.current.toDataURL("image/png");
+  const save = (e) => {
+    const img = canvasRef.current.toDataURL("image/png");
 
-		e.target.href = img;
-	};
+    e.target.href = img;
+  };
 
-	const handleName = useRef(
-		_.debounce((input) => {
-			if (!input) return setName("Jan Kowalski");
-			setName(input);
-		}, 500)
-	).current;
+  const handleName = useRef(
+    _.debounce((input) => {
+      if (!input) return setName("Jan Kowalski");
+      setName(input);
+    }, 500)
+  ).current;
 
-	const handleKlasa = useRef(
-		_.debounce((input) => {
-			if (!input) return setKlasa("1X");
-			setKlasa(input);
-		}, 500)
-	).current;
+  const handleKlasa = useRef(
+    _.debounce((input) => {
+      if (!input) return setKlasa("1X");
+      setKlasa(input);
+    }, 500)
+  ).current;
 
-	return (
-		<>
-			<div className="container">
-				<canvas ref={canvasRef} />
-				<input
-					placeholder="Imię i Nazwisko"
-					onChange={(e) => handleName(e.target.value)}
-				/>
-				<input
-					placeholder="Klasa"
-					onChange={(e) => handleKlasa(e.target.value)}
-				/>
-				<a
-					href="/tytulowa"
-					onClick={save}
-					download={`${name
-						.toLowerCase()
-						.replace(/ /g, "-")}-${klasa.toLowerCase()}.png`}
-				>
-					Zapisz
-				</a>
-			</div>
-			<footer>
-				<a href="https://github.com/zsk-poznan/tytulowa">
-					<img alt="GitHub Source Code" src="https://i.imgur.com/3PbBdr2.png" />
-				</a>
-				<br />
-				Made by <a href="https://ejer.ga">ejer</a>
-			</footer>
-		</>
-	);
+  return (
+    <>
+      <div className="container">
+        <div className="subjectChange">
+          <button onClick={() => setSubject("geo")}>Geografia</button>
+          <button onClick={() => setSubject("dg")}>
+            Działalność gospodarcza
+          </button>
+        </div>
+
+        <canvas ref={canvasRef} />
+        <input
+          placeholder="Imię i Nazwisko"
+          onChange={(e) => handleName(e.target.value)}
+        />
+        <input
+          placeholder="Klasa"
+          onChange={(e) => handleKlasa(e.target.value)}
+        />
+        <a
+          href="/tytulowa"
+          onClick={save}
+          download={`${name
+            .toLowerCase()
+            .replace(/ /g, "-")}-${klasa.toLowerCase()}.png`}
+        >
+          Zapisz
+        </a>
+      </div>
+      <footer>
+        <a href="https://github.com/zsk-poznan/tytulowa">
+          <img alt="GitHub Source Code" src="https://i.imgur.com/3PbBdr2.png" />
+        </a>
+        <br />
+        Made by <a href="https://ejer.ga">ejer</a>
+      </footer>
+    </>
+  );
 }
 
 export default App;
--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -1,60 +1,110 @@
 import { useEffect } from "react";
 
-const useCanvas = (canvasRef, name, klasa) => {
-	useEffect(() => {
-		const canvas = canvasRef.current;
-		const ctx = canvas.getContext("2d");
+const useCanvas = (canvasRef, name, klasa, subject) => {
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
-		let logo = new Image();
-		logo.src = "/tytulowa/logo.png";
+    let logo = new Image();
+    logo.src = "/tytulowa/logo.png";
 
-		canvas.width = 800;
-		canvas.height = 500;
+    canvas.width = 800;
+    canvas.height = 500;
 
-		ctx.fillStyle = "#ca4b4b";
-		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    if (subject === "geo") {
+      ctx.fillStyle = "#ca4b4b";
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-		ctx.fillStyle = "#9c2b2b";
-		ctx.fillRect(0, ctx.canvas.height - 100, ctx.canvas.width, 100);
+      ctx.fillStyle = "#9c2b2b";
+      ctx.fillRect(0, ctx.canvas.height - 100, ctx.canvas.width, 100);
 
-		logo.onload = () => {
-			ctx.drawImage(logo, 55, ctx.canvas.height - 150, 100, 100);
-		};
+      logo.onload = () => {
+        ctx.drawImage(logo, 55, ctx.canvas.height - 150, 100, 100);
+      };
 
-		ctx.fillStyle = "#ffffff";
+      ctx.fillStyle = "#ffffff";
 
-		ctx.font = "700 36px Montserrat";
-		ctx.fillText("Zespół Szkół Komunikacji", 175, ctx.canvas.height - 110);
+      ctx.font = "700 36px Montserrat";
+      ctx.fillText("Zespół Szkół Komunikacji", 175, ctx.canvas.height - 110);
 
-		ctx.font = "400 24px Montserrat";
-		ctx.fillText(
-			"im. Hipolita Cegielskiego w Poznaniu",
-			175,
-			ctx.canvas.height - 75
-		);
+      ctx.font = "400 24px Montserrat";
+      ctx.fillText(
+        "im. Hipolita Cegielskiego w Poznaniu",
+        175,
+        ctx.canvas.height - 75
+      );
 
-		ctx.textAlign = "center";
+      ctx.textAlign = "center";
 
-		ctx.font = "700 48px Montserrat";
-		ctx.fillText("Geografia", ctx.canvas.width / 2, 100);
+      ctx.font = "700 48px Montserrat";
+      ctx.fillText("Geografia", ctx.canvas.width / 2, 100);
 
-		ctx.font = "700 32px Montserrat";
-		ctx.fillText("Technikum Komunikacji", ctx.canvas.width / 2, 150);
+      ctx.font = "700 32px Montserrat";
+      ctx.fillText("Technikum Komunikacji", ctx.canvas.width / 2, 150);
 
-		ctx.font = "400 42px Montserrat";
-		ctx.fillText(name, ctx.canvas.width / 2, 250);
+      ctx.font = "400 42px Montserrat";
+      ctx.fillText(name, ctx.canvas.width / 2, 250);
 
-		ctx.font = "400 30px Montserrat";
-		ctx.fillText(`Klasa ${klasa}`, ctx.canvas.width / 2, 285);
+      ctx.font = "400 30px Montserrat";
+      ctx.fillText(`Klasa ${klasa}`, ctx.canvas.width / 2, 285);
 
-		const ratio =
-			window.screen.availWidth > 800
-				? 1
-				: window.screen.availWidth / canvas.width / 1.05;
+      const ratio =
+        window.screen.availWidth > 800
+          ? 1
+          : window.screen.availWidth / canvas.width / 1.05;
 
-		canvas.style.width = canvas.width * ratio + "px";
-		canvas.style.height = canvas.height * ratio + "px";
-	}, [canvasRef, name, klasa]);
+      canvas.style.width = canvas.width * ratio + "px";
+      canvas.style.height = canvas.height * ratio + "px";
+    } else if (subject === "dg") {
+      ctx.fillStyle = "#8c2222";
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+      ctx.fillStyle = "#151110";
+      ctx.fillRect(0, ctx.canvas.height - 100, ctx.canvas.width, 100);
+
+      logo.onload = () => {
+        ctx.drawImage(logo, 55, ctx.canvas.height - 150, 100, 100);
+      };
+
+      ctx.fillStyle = "#ffffff";
+
+      ctx.font = "700 36px Montserrat";
+      ctx.fillText("Zespół Szkół Komunikacji", 175, ctx.canvas.height - 110);
+
+      ctx.font = "400 24px Montserrat";
+      ctx.fillText(
+        "im. Hipolita Cegielskiego w Poznaniu",
+        175,
+        ctx.canvas.height - 75
+      );
+
+      ctx.textAlign = "center";
+
+      ctx.font = "700 48px Montserrat";
+      ctx.fillText("Działalność", ctx.canvas.width / 2, 75);
+      ctx.fillText("Gospodarcza", ctx.canvas.width / 2, 125);
+
+      ctx.font = "500 28px Montserrat";
+      ctx.fillText("„Twój własny spichlerz”", ctx.canvas.width / 2, 175);
+
+      ctx.font = "400 42px Montserrat";
+      ctx.fillText(name, ctx.canvas.width / 2, 250);
+
+      ctx.font = "400 30px Montserrat";
+      ctx.fillText(`Klasa ${klasa}`, ctx.canvas.width / 2, 285);
+
+      ctx.font = "400 26px Montserrat";
+      ctx.fillText(`Technikum Komunikacji`, ctx.canvas.width / 2, 320);
+
+      const ratio =
+        window.screen.availWidth > 800
+          ? 1
+          : window.screen.availWidth / canvas.width / 1.05;
+
+      canvas.style.width = canvas.width * ratio + "px";
+      canvas.style.height = canvas.height * ratio + "px";
+    }
+  }, [canvasRef, name, klasa, subject]);
 };
 
 export default useCanvas;
